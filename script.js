ffccee1ca7a4916e0bf9f8c7763bec27560554c2
lunarrover1 <lunarrover1@gmail.com>
2025-02-11 13:31:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Add event listener to donate button
+document.querySelector('.donate button').addEventListener('click', () => {
+  // Redirect to donation page
+  window.location.href = 'https://www.libertyvoicesuperpac.com/donate';
+});
